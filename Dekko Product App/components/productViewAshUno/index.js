31464e3015752d9c40f5a239cf1cb803e1e9114b
Josh Jones <josh.jones86@hotmail.com>
2016-12-04 00:41:33 +0000

    Changes made by Josh Jones (josh.jones86@hotmail.com).

---
+++ Dekko Product App/components/productViewAshUno/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+app.productViewAshUno = kendo.observable({
+    onShow: function() {
+        window.screen.lockOrientation('portrait');
+    },
+    afterShow: function() {}
+});
+
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+(function () {
+    app.productViewAshUno.set('title', 'Ashley Uno');
+})();
